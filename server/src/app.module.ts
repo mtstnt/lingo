a4116ea3345167ff443691e23c751a5640d023fc
mtstnt <matthew.sutanto0612@gmail.com>
2026-06-21 14:12:02 +0700

    setup(server): add setup for Docker, MikroORM, BullMQ

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { BullModule } from '@nestjs/bull';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        driver: PostgreSqlDriver,
+        clientUrl: config.getOrThrow('DATABASE_URL'),
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        redis: config.getOrThrow('REDIS_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
